3a03448160358073826345f365bc953c49189e5c
BSapundzhiev <bsapundjiev@gmail.com>
2022-02-12 00:29:33 +0200

    Added to repository

---
+++ README.md
@@ -0,0 +1,24 @@
+﻿*bmp280 device detection Linux module*
+
+bmp085, bmp180, bmp280 and bme280 i2c device detection module used alongside
+with latest Linux kernel driver in `drivers/iio/pressure/bmp280`
+
+It may be usefull when bmp sensor is connected to diffrent i2c busses
+for example via i2c-tiny-usb driver or just to avoid manual detection and 
+initialization.
+
+
+*Build*
+
+$ make 
+$ sudo make install 
+$ sudo depmod -a
+
+*Configuration*
+
+$ sudo modprobe bmp280-probe
+
+To detect sensor on startup add line "bmp280-probe" in /etc/modules 
+or use the command:
+
+$ echo "bmp280-probe" | sudo tee -a /etc/modules
---
+++ bmp280-probe.c
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2022  Borislav Sapundzhiev
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/device.h>
+#include <linux/i2c.h>
+
+#include "bmp280.h"
+
+/* module for explicit detect i2c device by bus id */
+#define	I2C_ADAPTERS_MAX			  (24)
+
+/* device probing addreses */
+/**\name I2C addresses */
+#define BME280_I2C_ADDR_PRIM                      (0x76)
+#define BME280_I2C_ADDR_SEC                       (0x77)
+
+static const unsigned short probe_i2c_addrs[] = 
+	I2C_ADDRS(BME280_I2C_ADDR_PRIM, BME280_I2C_ADDR_SEC);
+
+static struct i2c_board_info bmp280_i2c_info[] = {
+	{ 
+	  I2C_BOARD_INFO("bmp280", BMP280_CHIP_ID), 
+	  I2C_BOARD_INFO("bmp180", BMP180_CHIP_ID),
+	  I2C_BOARD_INFO("bmp085", BMP180_CHIP_ID),
+	  I2C_BOARD_INFO("bme280", BME280_CHIP_ID), 
+	},
+};
+
+static int bmp280_i2c_device_probe(void)
+{
+	unsigned short adapters_nr = 0;
+	pr_info("Probing i2c adapters: [%d-%d]\n", 
+				adapters_nr, I2C_ADAPTERS_MAX);
+
+	for (adapters_nr = adapters_nr; adapters_nr < I2C_ADAPTERS_MAX; adapters_nr++) {
+
+		struct i2c_adapter * i2c_adapter = i2c_get_adapter(adapters_nr);
+
+		if (i2c_adapter) {
+			struct i2c_client *i2c_device = 
+				i2c_new_probed_device(i2c_adapter, 
+							bmp280_i2c_info, probe_i2c_addrs, NULL);
+
+			if (i2c_device) {
+				pr_info("Found device: %s @ i2c-%d addr: 0x%x\n", 
+					i2c_device->name, adapters_nr, i2c_device->addr);
+				i2c_release_client(i2c_device);
+			}
+		}
+	}
+	return 0;
+}
+
+static int __init bmp280_probe_init(void)
+{
+	return bmp280_i2c_device_probe();
+}
+
+static void __exit bmp280_probe_exit(void)
+{
+	pr_info("%s\n", __FUNCTION__);
+}
+
+module_init(bmp280_probe_init);
+module_exit(bmp280_probe_exit);
+
+MODULE_AUTHOR("Borislav Sapundzhiev <BSapundzhiev@gmail.com>");
+MODULE_DESCRIPTION("bmp280 device detection");
+MODULE_LICENSE("GPL");
