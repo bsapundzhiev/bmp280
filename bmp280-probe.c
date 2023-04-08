cbe576653fdff2c34604f61503d34e03297f1f80
BSapundzhiev <bsapundjiev@gmail.com>
2023-04-08 22:44:19 +0300

    Code cleanup

--- bmp280-probe.c
+++ bmp280-probe.c
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * bmp280-probe.c bmp i2c device detection 
- * 
+ * bmp280-probe.c bmp i2c device detection
+ *
  * Copyright (c) 2022  Borislav Sapundzhiev <bsapunjiev@gmail.com>
  */
- 
+
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
 #include <linux/kernel.h>
@@ -22,43 +22,54 @@
 #define BME280_I2C_ADDR_PRIM                      (0x76)
 #define BME280_I2C_ADDR_SEC                       (0x77)
 
-static const unsigned short probe_i2c_addrs[] = 
+static const unsigned short probe_i2c_addrs[] =
 	I2C_ADDRS(BME280_I2C_ADDR_PRIM, BME280_I2C_ADDR_SEC);
 
 static struct i2c_board_info bmp280_i2c_info[] = {
-	{ 
-	  I2C_BOARD_INFO("bmp280", BMP280_CHIP_ID), 
+	{
+	  I2C_BOARD_INFO("bmp280", BMP280_CHIP_ID),
 	  I2C_BOARD_INFO("bmp180", BMP180_CHIP_ID),
 	  I2C_BOARD_INFO("bmp085", BMP180_CHIP_ID),
-	  I2C_BOARD_INFO("bme280", BME280_CHIP_ID), 
+	  I2C_BOARD_INFO("bme280", BME280_CHIP_ID),
 	},
 };
 
-static int bmp280_i2c_device_probe(void)
+static int bmp280_i2c_device_probe(struct i2c_adapter *i2c_adapter,
+				   unsigned short adapter)
 {
-	unsigned short adapters_nr = 0;
-	pr_info("Probing i2c adapters: [%d-%d]\n", 
-				adapters_nr, I2C_ADAPTERS_MAX);
-
-	for (adapters_nr = adapters_nr; adapters_nr <= I2C_ADAPTERS_MAX; adapters_nr++) {
-		struct i2c_adapter *i2c_adapter = i2c_get_adapter(adapters_nr);
-		if (i2c_adapter) {
-			struct i2c_client *i2c_device = 
-				i2c_new_scanned_device(i2c_adapter, 
+	struct i2c_client *i2c_device = i2c_new_scanned_device(i2c_adapter,
 					bmp280_i2c_info, probe_i2c_addrs, NULL);
-			if (!IS_ERR(i2c_device)) {
-				pr_info("Found device: %s @ i2c-%d addr: 0x%x\n", 
-					i2c_device->name, adapters_nr, i2c_device->addr);
-			}
-		}
 
+	if (IS_ERR(i2c_device)) {
+		return PTR_ERR(i2c_device);
 	}
+
+	pr_info("Found device: %s @ i2c-%d addr: 0x%x\n",
+		i2c_device->name, adapter, i2c_device->addr);
+
+	return 0;
+}
+
+static int bmp280_i2c_devices_probe(void)
+{
+	unsigned short adapter = 0;
+	pr_info("Probing i2c adapters: [%d-%d]\n", adapter, I2C_ADAPTERS_MAX);
+
+	for (adapter = adapter; adapter <= I2C_ADAPTERS_MAX; adapter++) {
+
+		struct i2c_adapter *i2c_adapter = i2c_get_adapter(adapter);
+
+		if (i2c_adapter) {
+			bmp280_i2c_device_probe(i2c_adapter, adapter);
+		}
+	}
+
 	return 0;
 }
 
 static int __init bmp280_probe_init(void)
 {
-	return bmp280_i2c_device_probe();
+	return bmp280_i2c_devices_probe();
 }
 
 static void __exit bmp280_probe_exit(void)
